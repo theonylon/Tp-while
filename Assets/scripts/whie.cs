bc1c4dca2b6e575e9890939d4cc75e0cc221461b
theonlyone <102159023+theonylon@users.noreply.github.com>
2022-04-21 09:04:10 -0300

    tp while started

--- Assets/scripts/whie.cs
+++ Assets/scripts/whie.cs
@@ -6,8 +6,12 @@ public class whie : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
+        public int counter, countermax;
+        public TextAlignment txt_display;
     {
-        
+     while (counter countermax){
+
+        } 
     }
 
     // Update is called once per frame
